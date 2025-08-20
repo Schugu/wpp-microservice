b231994d568b198ab76c4fc97a4d8d6129e82ab9
Schugu <leo.schugu@gmail.com>
2025-08-20 19:09:54 -0300

    add: schema con formateador de números

--- src/modules/whatsapp/schemas/number.schema.ts
+++ src/modules/whatsapp/schemas/number.schema.ts
@@ -1,6 +1,14 @@
 import { z } from "zod";
 import * as validator from "@/utils/validators";
+import { formatPhoneNumberPlain } from "@/utils/formatPhoneNumber";
 
 export const numberSchema = z.object({
-  number: validator.string(8,15),
+  number: z.string({
+    required_error: "El número es requerido.",
+    invalid_type_error: "El número debe ser un string."
+  })
+    .transform((val) => formatPhoneNumberPlain(val)) 
+    .refine((val) => /^\d{8,15}$/.test(val), { 
+      message: "El número debe contener solo dígitos y tener entre 8 y 15 caracteres.",
+    }),
 }).strict();
