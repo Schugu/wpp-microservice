e06489779dd3f31de357ce5828fb204b0fb928f6
Schugu <leo.schugu@gmail.com>
2025-08-20 19:10:39 -0300

    style: code

--- src/modules/whatsapp/routes/session.routes.ts
+++ src/modules/whatsapp/routes/session.routes.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import * as controller from "../controllers/session";
-export const sessionRoutes = express.Router();
 import { validateParams } from "@/middlewares/validateSchema";
 import { numberSchema } from "../schemas/number.schema";
+
+export const sessionRoutes = express.Router();
 
 sessionRoutes.post("/:number", validateParams(numberSchema), controller.create);
 sessionRoutes.get('/status/:number', validateParams(numberSchema), controller.checkStatus)
